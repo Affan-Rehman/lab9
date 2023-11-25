8a608f397344286401524786c860af3276d2cae2
Affan Rehman <112381046+Affan-Rehman@users.noreply.github.com>
2023-11-25 11:36:16 +0500

    changes in poem method, Complete test file

--- src/poet/GraphPoet.java
+++ src/poet/GraphPoet.java
@@ -1,63 +1,13 @@
-/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
- * Redistribution of original or derived work requires permission of course staff.
- */
 package poet;
-
-import graph.Graph;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-/**
- * A graph-based poetry generator.
- * 
- * <p>GraphPoet is initialized with a corpus of text, which it uses to derive a
- * word affinity graph.
- * Vertices in the graph are words. Words are defined as non-empty
- * case-insensitive strings of non-space non-newline characters. They are
- * delimited in the corpus by spaces, newlines, or the ends of the file.
- * Edges in the graph count adjacencies: the number of times "w1" is followed by
- * "w2" in the corpus is the weight of the edge from w1 to w2.
- * 
- * <p>For example, given this corpus:
- * <pre>    Hello, HELLO, hello, goodbye!    </pre>
- * <p>the graph would contain two edges:
- * <ul><li> ("hello,") -> ("hello,")   with weight 2
- *     <li> ("hello,") -> ("goodbye!") with weight 1 </ul>
- * <p>where the vertices represent case-insensitive {@code "hello,"} and
- * {@code "goodbye!"}.
- * 
- * <p>Given an input string, GraphPoet generates a poem by attempting to
- * insert a bridge word between every adjacent pair of words in the input.
- * The bridge word between input words "w1" and "w2" will be some "b" such that
- * w1 -> b -> w2 is a two-edge-long path with maximum-weight weight among all
- * the two-edge-long paths from w1 to w2 in the affinity graph.
- * If there are no such paths, no bridge word is inserted.
- * In the output poem, input words retain their original case, while bridge
- * words are lower case. The whitespace between every word in the poem is a
- * single space.
- * 
- * <p>For example, given this corpus:
- * <pre>    This is a test of the Mugar Omni Theater sound system.    </pre>
- * <p>on this input:
- * <pre>    Test the system.    </pre>
- * <p>the output poem would be:
- * <pre>    Test of the system.    </pre>
- * 
- * <p>PS2 instructions: this is a required ADT class, and you MUST NOT weaken
- * the required specifications. However, you MAY strengthen the specifications
- * and you MAY add additional methods.
- * You MUST use Graph in your rep, but otherwise the implementation of this
- * class is up to you.
- */
+import java.util.*;
+
 public class GraphPoet {
 
-    private final Map<String, Map<String, Integer>> wordGraph = new HashMap<>();
+    private final Map<String, Map<String, Integer>> graph = new HashMap<>();
 
     public GraphPoet(File corpus) throws IOException {
         List<String> words = Files.readAllLines(corpus.toPath());
@@ -65,43 +15,44 @@ public class GraphPoet {
     }
 
     public String poem(String input) {
-        List<String> inputWords = Arrays.asList(input.split("\\s+"));
-        StringBuilder poemBuilder = new StringBuilder();
+        String[] inputWords = input.split("\\s+");
+        List<String> poemWords = new ArrayList<>();
 
-        for (int i = 0; i < inputWords.size() - 1; i++) {
-            String currentWord = inputWords.get(i).toLowerCase();
-            String nextWord = inputWords.get(i + 1).toLowerCase();
+        for (int i = 0; i < inputWords.length - 1; i++) {
+            String currentWord = inputWords[i].toLowerCase();
+            String nextWord = inputWords[i + 1].toLowerCase();
 
-            poemBuilder.append(inputWords.get(i)).append(" ");
+            poemWords.add(currentWord);
             String bridgeWord = findBridgeWord(currentWord, nextWord);
             if (!bridgeWord.isEmpty()) {
-                poemBuilder.append(bridgeWord).append(" ");
+                poemWords.add(bridgeWord.toLowerCase());
             }
         }
-        poemBuilder.append(inputWords.get(inputWords.size() - 1));
+        poemWords.add(inputWords[inputWords.length - 1].toLowerCase());
 
-        return poemBuilder.toString();
+        return String.join(" ", poemWords);
     }
 
     private String findBridgeWord(String word1, String word2) {
-        if (!wordGraph.containsKey(word1) || !wordGraph.containsKey(word2)) {
+        if (!graph.containsKey(word1) || !graph.containsKey(word2)) {
             return "";
         }
 
-        Map<String, Integer> connections = wordGraph.get(word1);
-        int maxWeight = 0;
+        Map<String, Integer> word1Connections = graph.get(word1);
         String bridgeWord = "";
+        int maxWeight = -1;
 
-        for (String word : connections.keySet()) {
-            if (wordGraph.containsKey(word) && wordGraph.get(word).containsKey(word2)) {
-                int weight = connections.get(word);
+        for (Map.Entry<String, Integer> entry : word1Connections.entrySet()) {
+            String target = entry.getKey();
+            int weight = entry.getValue();
+
+            if (graph.containsKey(target) && graph.get(target).containsKey(word2)) {
                 if (weight > maxWeight) {
                     maxWeight = weight;
-                    bridgeWord = word;
+                    bridgeWord = target;
                 }
             }
         }
-
         return bridgeWord;
     }
 
@@ -110,8 +61,8 @@ public class GraphPoet {
             String currentWord = words.get(i).toLowerCase();
             String nextWord = words.get(i + 1).toLowerCase();
 
-            wordGraph.putIfAbsent(currentWord, new HashMap<>());
-            Map<String, Integer> connections = wordGraph.get(currentWord);
+            graph.putIfAbsent(currentWord, new HashMap<>());
+            Map<String, Integer> connections = graph.get(currentWord);
             connections.put(nextWord, connections.getOrDefault(nextWord, 0) + 1);
         }
     }
--- test/poet/GraphPoetTest.java
+++ test/poet/GraphPoetTest.java
@@ -1,25 +1,65 @@
-/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
- * Redistribution of original or derived work requires permission of course staff.
- */
 package poet;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * Tests for GraphPoet.
- */
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class GraphPoetTest {
-    
-    // Testing strategy
-    //   TODO
-    
-    @Test(expected=AssertionError.class)
+
+    @Test(expected = AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
-    
-    // TODO tests
-    
+
+    @Test
+    public void testGraphPoetCreationFromFile() throws IOException {
+        File tempFile = File.createTempFile("test_corpus", ".txt");
+        GraphPoet graphPoet = new GraphPoet(tempFile);
+        assertNotNull(graphPoet);
+        tempFile.delete();
+    }
+    @Test
+    public void testPoemGenerationDifferentInput() throws IOException {
+        // Create a temporary corpus file for testing
+        File tempFile = File.createTempFile("test_corpus", ".txt");
+        String corpusContent = "This is a beautiful day in the park.";
+        Files.write(tempFile.toPath(), corpusContent.getBytes());
+
+        // Create GraphPoet instance using the temporary corpus file
+        GraphPoet graphPoet = new GraphPoet(tempFile);
+
+        // Define input and expected poem
+        String input = "a beautiful day";
+        String expectedPoem = "a beautiful day";
+
+        // Generate poem and check if it matches the expected output
+        String poem = graphPoet.poem(input);
+        assertEquals(expectedPoem, poem);
+
+        // Delete the temporary file after the test
+        tempFile.delete();
+    }
+
+    @Test
+    public void testEmptyInputPoemGeneration() throws IOException {
+        File tempFile = File.createTempFile("test_corpus", ".txt");
+        GraphPoet graphPoet = new GraphPoet(tempFile);
+
+        String input = "";
+        String expectedPoem = "";
+        String poem = graphPoet.poem(input);
+
+        assertEquals(expectedPoem, poem);
+        tempFile.delete();
+    }
+
+    @Test(expected = IOException.class)
+    public void testInvalidFile() throws IOException {
+        File nonExistentFile = new File("nonexistent.txt");
+        GraphPoet graphPoet = new GraphPoet(nonExistentFile);
+    }
 }
